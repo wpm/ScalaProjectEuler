8f9fb88d37c8f7dbf3e0f687dffb483b6d16812d
W.P. McNeill <billmcn@gmail.com>
2012-09-18 21:27:15 -0700

    Single line stream iterator for Collatz Chain

--- src/test/scala/projecteuler.net/CollatzSpec.scala
+++ src/test/scala/projecteuler.net/CollatzSpec.scala
@@ -4,27 +4,27 @@ import org.scalatest.FlatSpec
 
 class CollatzSpec extends FlatSpec {
   "Collatz(1)" should "be 1" in {
-    expect(1) {
+    expect(Nil) {
       Collatz.collatz(1)
     }
   }
   "Collatz(2)" should "be 2" in {
-    expect(2) {
+    expect(Seq(2)) {
       Collatz.collatz(2)
     }
   }
-  "Collatz(3)" should "be 8" in {
-    expect(8) {
+  "Collatz(3)" should "be 3, 10, 5, 16, 8, 4, 2" in {
+    expect(Seq(3, 10, 5, 16, 8, 4, 2)) {
       Collatz.collatz(3)
     }
   }
-  "Collatz(4)" should "be 3" in {
-    expect(3) {
+  "Collatz(4)" should "be 4, 2" in {
+    expect(Seq(4, 2)) {
       Collatz.collatz(4)
     }
   }
-  "Collatz(13)" should "be 10" in {
-    expect(10) {
+  "Collatz(13)" should "be 13, 40, 20, 10, 5, 16, 8, 4, 2" in {
+    expect(Seq(13, 40, 20, 10, 5, 16, 8, 4, 2)) {
       Collatz.collatz(13)
     }
   }
