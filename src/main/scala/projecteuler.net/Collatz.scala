50c3a671e47229f33409316c9017afc343d9da52
W.P. McNeill <billmcn@gmail.com>
2012-09-18 22:36:23 -0700

    Collatz Chain with Length Caching

--- src/main/scala/projecteuler.net/Collatz.scala
+++ src/main/scala/projecteuler.net/Collatz.scala
@@ -1,6 +1,35 @@
 package projecteuler.net
 
+import collection.mutable
 
-object Collatz {
-  val collatz = Stream.iterate(_: BigInt)(n => if (n % 2 == 0) n / 2 else 3 * n + 1).takeWhile(_ > 1)
+class Collatz {
+  /**
+   * Hailstone chain
+   */
+  val hailstone = Stream.iterate(_: BigInt)(n => if (n % 2 == 0) n / 2 else 3 * n + 1).takeWhile(_ > 1) :+ BigInt("1")
+  /**
+   * Map from a start value to the length of the corresponding Hailstone chain
+   */
+  private val cachedLength = mutable.Map[BigInt, Int](BigInt("1") -> 1)
+
+  /**
+   * The length of a Hailstone chain
+   *
+   * This has the side effect of updating the length cache in this instance.
+   *
+   * @param start the starting value
+   * @return length of the chain starting at the specified value
+   */
+  def chainLength(start: BigInt): Int = {
+    // Run the hailstone chain from the starting value until the first cached length.
+    val chain = hailstone(start)
+    val newChain = chain.takeWhile(n => !cachedLength.contains(n))
+    // Add the new chain elements the the length cache.
+    val l = newChain.length
+    val d = cachedLength(chain(l))
+    for ((n, i) <- newChain.take(newChain.length).zipWithIndex) cachedLength(n) = d + l - i
+    l + d
+  }
+
+  override def toString = cachedLength.mkString("(", ", ", ")")
 }
--- src/test/scala/projecteuler.net/CollatzSpec.scala
+++ src/test/scala/projecteuler.net/CollatzSpec.scala
@@ -3,29 +3,33 @@ package projecteuler.net
 import org.scalatest.FlatSpec
 
 class CollatzSpec extends FlatSpec {
-  "Collatz(1)" should "be 1" in {
-    expect(Nil) {
-      Collatz.collatz(1)
+  def fixture = new {
+    val collatz = new Collatz
+  }
+
+  "Hailstone(1)" should "be 1" in {
+    expect(Seq(1)) {
+      fixture.collatz.hailstone(1)
     }
   }
-  "Collatz(2)" should "be 2" in {
-    expect(Seq(2)) {
-      Collatz.collatz(2)
+  "Hailstone(2)" should "be 2, 1" in {
+    expect(Seq(2, 1)) {
+      fixture.collatz.hailstone(2)
     }
   }
-  "Collatz(3)" should "be 3, 10, 5, 16, 8, 4, 2" in {
-    expect(Seq(3, 10, 5, 16, 8, 4, 2)) {
-      Collatz.collatz(3)
+  "Hailstone(3)" should "be 3, 10, 5, 16, 8, 4, 2, 1" in {
+    expect(Seq(3, 10, 5, 16, 8, 4, 2, 1)) {
+      fixture.collatz.hailstone(3)
     }
   }
-  "Collatz(4)" should "be 4, 2" in {
-    expect(Seq(4, 2)) {
-      Collatz.collatz(4)
+  "Hailstone(4)" should "be 4, 2, 1" in {
+    expect(Seq(4, 2, 1)) {
+      fixture.collatz.hailstone(4)
     }
   }
-  "Collatz(13)" should "be 13, 40, 20, 10, 5, 16, 8, 4, 2" in {
-    expect(Seq(13, 40, 20, 10, 5, 16, 8, 4, 2)) {
-      Collatz.collatz(13)
+  "Hailstone(13)" should "be 13, 40, 20, 10, 5, 16, 8, 4, 2, 1" in {
+    expect(Seq(13, 40, 20, 10, 5, 16, 8, 4, 2, 1)) {
+      fixture.collatz.hailstone(13)
     }
   }
 }
